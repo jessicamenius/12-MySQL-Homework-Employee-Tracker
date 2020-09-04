3a7fda2acfb634703206c1ab601534908373d17c
Jessica <jessicamenius@gmail.com>
2020-09-04 15:10:34 -0500

    updating with .env file

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,11 @@
 const mysql = require("mysql");
+const env = require("dotenv").config();
+
 const connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "NugPup28@",
+  password: process.env.SECRET,
   database: "employee_db",
 });
 
